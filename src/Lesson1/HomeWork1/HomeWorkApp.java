e876c654df8f09ccc54f1290fc69ecacddde1a62
IlyaKon <ilya.997@yandex.ru>
2021-10-22 14:49:01 +0700

    Сделал домашку 1
    Добавил в .gitignore папку "out"

--- src/Lesson1/HomeWork1/HomeWorkApp.java
+++ src/Lesson1/HomeWork1/HomeWorkApp.java
@@ -1,4 +1,48 @@
 package Lesson1.HomeWork1;
 
 public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+checkSumSign();
+printColor();
+        compareNumbers();
+
+    }
+    public static void printThreeWords(){
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+
+    }
+    public  static void checkSumSign(){
+        int a = 398;
+        int b = -789;
+        int result = a+b;
+        if (result >= 0 ){
+            System.out.println("Сумма положительная");}
+        else
+            System.out.println("Сумма отрицательная");
+
+    }
+    public  static void printColor(){
+        int value = 101;
+        if ( value <= 0){
+            System.out.println("Красный");
+        }
+        else if (0< value && value<=100){
+            System.out.println("Жёлтый");
+        }
+        else
+            System.out.println("Зелёный");
+    }
+    public  static void compareNumbers(){
+        int a = 589;
+        int b = 980;
+        if (a >= b){
+            System.out.println("a >= b");
+        }
+        else
+            System.out.println("a < b");
+
+    }
 }
